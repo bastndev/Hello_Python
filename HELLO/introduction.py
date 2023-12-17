b490b6f27f344afcecfcdc9b7afff784bf5b3227
bastndev <gohit24@gmail.com>
2023-12-16 21:43:40 -0500

    :arrow_up: Update `new`

--- HELLO/introduction.py
+++ HELLO/introduction.py
@@ -34,7 +34,16 @@ name = True
 
 print(name) """
 
-var_name = "Snake case"
+""" var_name = "Snake case"
 Var_Name = "Pascal case"
 var_name = "camel case"
-varName = "camel case"
+varName = "camel case" """
+
+""" Keyword (False      await      else       import     pass
+None       break      except     in         raise
+True       class      finally    is         return
+and        continue   for        lambda     try
+as         def        from       nonlocal   while
+assert     del        global     not        with
+async      elif       if         or         yield) """
+
