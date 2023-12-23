80c55060f0e61f6f68c560028b86c6f31c3113b8
bastndev <gohit24@gmail.com>
2023-12-23 17:07:30 -0500

    :arrow_up: Update `new`

--- DAY 1/exercise.py
+++ DAY 1/exercise.py
@@ -1,7 +1,18 @@
-a = input("Enter first number: ")
+""" a = input("Enter first number: ")
 b = input("Enter second number: ")
 
 num1 = ("a: " + b)
 num2 = ("b: " + a)
 print(num1)
 print(num2)
+"""
+
+a = input("Enter first number: ")
+b = input("Enter second number: ")
+
+c = a 
+a = b
+b = c
+
+print("a: " + a)
+print("b: " + b)
