1242969b60a46fc3d3d738d3c558e197fa0d1e8e
bastndev <gohit24@gmail.com>
2023-12-23 11:24:57 -0500

    :arrow_up: Update `new`

--- DAY 1/mian.py
+++ DAY 1/mian.py
@@ -1,3 +1,6 @@
-print("Hello, world!")
+""" print("Hello, world!")
 print("print('Hello,\n Welcome 2024')")
-print("Hello \nMr. Lucas")
+print("Hello \nMr. Lucas") """
+
+print("Hello" + "world!")
+print("Hello" +" "+ "world!")
