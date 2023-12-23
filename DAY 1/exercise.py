d79f18d32c7d3ec4e25fe8a738c23cf0222068f5
bastndev <gohit24@gmail.com>
2023-12-23 17:55:19 -0500

    :arrow_up: Update `new`

--- DAY 1/exercise.py
+++ DAY 1/exercise.py
@@ -7,7 +7,7 @@ print(num1)
 print(num2)
 """
 
-a = input("Enter first number: ")
+""" a = input("Enter first number: ")
 b = input("Enter second number: ")
 
 c = a 
@@ -16,3 +16,27 @@ b = c
 
 print("a: " + a)
 print("b: " + b)
+"""
+
+# -TODO: Exercise 16. Day 1 Project: Band Name Generator
+
+# print("Hello, world!")
+
+""" city = input("Enter country: ")
+print(f"Beautiful city {city}") """
+
+""" pet = input("Give me name of pet: ")
+print(pet) """
+
+""" 
+city = input("Enter country: ")
+
+pet = input("Give me name of pet: ")
+
+print(f"The name of your band is: {city}{pet}") """
+
+
+city = input("Enter Your City: \n")
+pet = input("What is the name of a pet? \n")
+
+print(f"Your band name could be: " + city + " " + pet)
