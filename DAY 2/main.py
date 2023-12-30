d3ce7416fb0e5729e5922f84d6f18c70fc4ca34b
bastndev <gohit24@gmail.com>
2023-12-30 10:09:08 -0500

    :arrow_up: Update `new`

--- DAY 2/main.py
+++ DAY 2/main.py
@@ -1,3 +1,20 @@
+""" String """
 # print(len("Hello"))
 
 # print('Hello'[1])
+
+# print("123" + "456")
+
+
+""" Integer """
+# print(123 + 456)
+
+# 123_456_789  | _ = ,
+
+""" Float """
+# 3.14159
+
+""" Boolean """
+# true or 
+# false
+
