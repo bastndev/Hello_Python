faf0328df945a1ee91f5519911e67b523f639230
bastndev <gohit24@gmail.com>
2023-12-30 11:57:58 -0500

    :arrow_up: Update `new`

--- DAY 2/exercise.py
+++ DAY 2/exercise.py
@@ -5,7 +5,7 @@ number2 = float(number[1])
 
 print(int(number1 + number2) ) """
 
-try:
+""" try:
     number = input("Enter the number: ")
     number1 = float(number[0])
     number2 = float(number[1])
@@ -18,4 +18,12 @@ except ValueError:
 except IndexError:
     print("Error: Please enter at least two digits.")
 except Exception as e:
-    print(f"An unexpected error occurred: {e}")
+    print(f"An unexpected error occurred: {e}") """
+
+
+""" number = input("Enter the number: ")
+number1 = int(number[0])
+number2 = int(number[1])
+
+result = (number1 + number2)
+print(f"The result is: {result}") """
