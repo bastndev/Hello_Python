fcf737bc55db7b5206318976a02b29e4026c14fb
bastndev <gohit24@gmail.com>
2023-12-30 15:58:35 -0500

    :arrow_up: Update `new`

--- DAY 2/number_manipulation.py
+++ DAY 2/number_manipulation.py
@@ -1,7 +1,10 @@
 # Use round to truncate the decimals."
-print(8 / 3)
+""" print(8 / 3)
 print(round(8 / 3))
 
 result = 8 / 3
 formatted_result = round(result, 2)
-print(formatted_result)
+print(formatted_result) """
+
+print(round(8 / 3, 2))
+print(round(8 / 3, 3))
