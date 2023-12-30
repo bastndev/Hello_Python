4261fdabb7eb968044e065050a89a69596dca850
bastndev <gohit24@gmail.com>
2023-12-30 18:09:58 -0500

    :arrow_up: Update `new`

--- DAY 2/day.py
+++ DAY 2/day.py
@@ -1,10 +1,9 @@
-print("> Welcome to the tip calculators")
-bill = input("What was the total bill?: $")
-porcent = input("Whats percentage tip would you like to give? 10, 12, or 15?")
-person = input("How many people to split the bill?")
+""" print("> Welcome to the tip calculators")
 
-bill_add =  float(bill)
-porcent =  bill_add / int(porcent)
-person1= int(person) / bill
-result = person1 + bill
-print(f"Each person should pay: {result}")
+bill = float(input("What was the total bill?: $"))
+tip = int(input("Whats percentage tip would you like to give? 10, 12, or 15? "))
+people = int(input("How many people to split the bill? "))
+
+bill_with_tip =  tip / 100  * bill + bill / people
+print(f"Each person should pay: {bill_with_tip:1f}")
+ """
