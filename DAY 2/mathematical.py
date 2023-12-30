d3b6a000f61fb80618ca8c2ec5c17a26421990b5
bastndev <gohit24@gmail.com>
2023-12-30 15:01:04 -0500

    :arrow_up: Update `new`

--- DAY 2/mathematical.py
+++ DAY 2/mathematical.py
@@ -4,8 +4,10 @@
 3 + 5
 7 - 4
 3 * 2
-6 / 2 
-2 ** 3 #raised to power. exponentiation^
+6 / 2
+2**3  # raised to power. exponentiation^
+
+
 # print(6 / 2)
 # print(2 * 2)
 
@@ -13,8 +15,12 @@
 """PEMDAS  """
 # Parentheses ()
 # Exponents **
-# Multiplication * 
+# Multiplication *
 # Division /
 # Additions +
 # Subtractions -
 
+
+# print(3 * 3 + 3 / 3 - 3)
+# print(3 / 3 + 3 * 3 - 3)
+print(3 * (3 + 3) / 3 - 3)
