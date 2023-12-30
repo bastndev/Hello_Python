6f24abee508883e10a5d4f30932885233c81884c
bastndev <gohit24@gmail.com>
2023-12-30 14:52:16 -0500

    :arrow_up: Update `new`

---
+++ DAY 2/mathematical.py
@@ -0,0 +1,20 @@
+# print(str(70) + str(100))
+
+""" Mathematical """
+3 + 5
+7 - 4
+3 * 2
+6 / 2 
+2 ** 3 #raised to power. exponentiation^
+# print(6 / 2)
+# print(2 * 2)
+
+
+"""PEMDAS  """
+# Parentheses ()
+# Exponents **
+# Multiplication * 
+# Division /
+# Additions +
+# Subtractions -
+
