9e471d9146a18a5d19b71f25138ae03744bfa585
bastndev <gohit24@gmail.com>
2023-12-30 15:59:15 -0500

    :arrow_up: Update `new`

--- DAY 2/number_manipulation.py
+++ DAY 2/number_manipulation.py
@@ -6,5 +6,7 @@ result = 8 / 3
 formatted_result = round(result, 2)
 print(formatted_result) """
 
-print(round(8 / 3, 2))
-print(round(8 / 3, 3))
+""" print(round(8 / 3, 2))
+print(round(8 / 3, 3)) """
+
+print(round(2.222222222222, 2))
