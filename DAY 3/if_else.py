d8ca22599591b8b15c20032836d282753138c5cf
bastndev <gohit24@gmail.com>
2024-01-20 09:47:28 -0500

    :arrow_up: Update `new 2.0`

---
+++ DAY 3/if_else.py
@@ -0,0 +1,7 @@
+print("Welcome to the rollercoaster!")
+height = float(input("Was is your height in cm? "))
+
+if height > 1.20:
+    print("Please enter")
+else:
+    print("sorry you don't enters")
