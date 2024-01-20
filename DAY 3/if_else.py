28fe5e3fd2a9169aa5c5b9804f1d0b86f7fcba61
bastndev <gohit24@gmail.com>
2024-01-20 10:42:19 -0500

    :arrow_up: Update `new 2.0`

--- DAY 3/if_else.py
+++ DAY 3/if_else.py
@@ -1,7 +1,23 @@
+""" print("Welcome to the rollercoaster!")
+height = float(input("Was is your height in cm? "))
+
+if height >= 1.20:
+    print("Please enter")
+else:
+    print("sorry you don't enters")
+"""
+
 print("Welcome to the rollercoaster!")
 height = float(input("Was is your height in cm? "))
 
 if height >= 1.20:
     print("Please enter")
+    age = int(input("How old are you: "))
+    if age < 12:
+        print("please pay 5$")
+    elif age <= 18:
+        print("please pay 7$")
+    else:
+        print("please pay 12$")
 else:
     print("sorry you don't enters")
